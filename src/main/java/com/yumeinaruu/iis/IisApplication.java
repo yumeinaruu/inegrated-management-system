fdacec5f02bf089f94ff08ef755047777ab722bc
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-22 15:17:05 +0300

    application configuration(pom and application.properties)

---
+++ src/main/java/com/yumeinaruu/iis/IisApplication.java
@@ -0,0 +1,13 @@
+package com.yumeinaruu.iis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IisApplication.class, args);
+    }
+
+}
