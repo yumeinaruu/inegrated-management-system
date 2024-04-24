15543fd0d1524ba082d962c525b02baa91e7fe32
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-24 22:47:31 +0300

    Faculty controller added

---
+++ src/main/java/com/yumeinaruu/iis/controller/FacultyController.java
@@ -0,0 +1,80 @@
+package com.yumeinaruu.iis.controller;
+
+import com.yumeinaruu.iis.exception.custom_exception.CustomValidationException;
+import com.yumeinaruu.iis.model.Faculty;
+import com.yumeinaruu.iis.model.dto.faculty.FacultyCreateDto;
+import com.yumeinaruu.iis.model.dto.faculty.FacultyUpdateDto;
+import com.yumeinaruu.iis.service.FacultyService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    @Autowired
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Faculty>> getAllFaculties() {
+        List<Faculty> faculties = facultyService.getAllFaculties();
+        if (faculties.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(faculties, HttpStatus.OK);
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<List<Faculty>> getAllFacultiesByName() {
+        List<Faculty> faculties = facultyService.getFacultiesSortedByName();
+        if (faculties.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(faculties, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Faculty> getFacultyById(@PathVariable Long id) {
+        Optional<Faculty> faculty = facultyService.getFacultyById(id);
+        if (faculty.isPresent()) {
+            return new ResponseEntity<>(faculty.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> createFaculty(@RequestBody @Valid FacultyCreateDto facultyCreateDto,
+                                                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(facultyService.createFaculty(facultyCreateDto) ? HttpStatus.CREATED : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> updateFaculty(@RequestBody @Valid FacultyUpdateDto facultyUpdateDto) {
+        return new ResponseEntity<>(facultyService.updateFaculty(facultyUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteFacultyById(@PathVariable Long id) {
+        return new ResponseEntity<>(facultyService.deleteFaculty(id) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+}
