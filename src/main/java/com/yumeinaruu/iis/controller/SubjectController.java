8286e18541edffce0ce85868265eff6ec29797ab
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-27 15:16:30 +0300

    subject controller added

---
+++ src/main/java/com/yumeinaruu/iis/controller/SubjectController.java
@@ -0,0 +1,105 @@
+package com.yumeinaruu.iis.controller;
+
+import com.yumeinaruu.iis.exception.custom_exception.CustomValidationException;
+import com.yumeinaruu.iis.model.Subject;
+import com.yumeinaruu.iis.model.dto.subject.SubjectCreateDto;
+import com.yumeinaruu.iis.model.dto.subject.SubjectDepartmentUpdateDto;
+import com.yumeinaruu.iis.model.dto.subject.SubjectNameUpdateDto;
+import com.yumeinaruu.iis.model.dto.subject.SubjectUpdateDto;
+import com.yumeinaruu.iis.service.SubjectService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+    private final SubjectService subjectService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Subject>> getAllSubjects() {
+        List<Subject> subjects = subjectService.getAllSubjects();
+        if (subjects.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(subjects, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Subject> getSubjectById(@PathVariable Long id) {
+        Optional<Subject> subject = subjectService.getSubjectById(id);
+        if (subject.isPresent()) {
+            return new ResponseEntity<>(subject.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<List<Subject>> getSubjectsSortedByName() {
+        List<Subject> subjects = subjectService.getSubjectsSortedByName();
+        if (subjects.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(subjects, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> createSubject(@RequestBody @Valid SubjectCreateDto subjectCreateDto,
+                                                    BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(subjectService.createSubject(subjectCreateDto) ? HttpStatus.CREATED : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> updateSubject(@RequestBody @Valid SubjectUpdateDto subjectUpdateDto,
+                                                    BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(subjectService.updateSubject(subjectUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/name")
+    public ResponseEntity<HttpStatus> updateSubjectName(@RequestBody @Valid SubjectNameUpdateDto subjectNameUpdateDto,
+                                                        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(subjectService.updateSubjectName(subjectNameUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/department")
+    public ResponseEntity<HttpStatus> updateSubjectDepartment(@RequestBody @Valid SubjectDepartmentUpdateDto subjectDepartmentUpdateDto,
+                                                              BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(subjectService.updateSubjectDepartment(subjectDepartmentUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteSubjectById(@PathVariable Long id) {
+        return new ResponseEntity<>(subjectService.deleteSubject(id) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+}
