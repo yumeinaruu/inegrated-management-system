48b0cd3429e0602a9eac2c82d5e1ea42d82c2479
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-27 15:53:25 +0300

    User Service added

---
+++ src/main/java/com/yumeinaruu/iis/service/UsersService.java
@@ -0,0 +1,103 @@
+package com.yumeinaruu.iis.service;
+
+import com.yumeinaruu.iis.model.Users;
+import com.yumeinaruu.iis.model.dto.users.UsersCreateDto;
+import com.yumeinaruu.iis.model.dto.users.UsersUpdateDto;
+import com.yumeinaruu.iis.model.dto.users.UsersUpdateGroupDto;
+import com.yumeinaruu.iis.model.dto.users.UsersUsernameUpdateDto;
+import com.yumeinaruu.iis.repository.GroupRepository;
+import com.yumeinaruu.iis.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+    private final GroupRepository groupRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository, GroupRepository groupRepository) {
+        this.usersRepository = usersRepository;
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public Optional<Users> getUserById(Long id) {
+        return usersRepository.findById(id);
+    }
+
+    public List<Users> getUsersSortedByUsername() {
+        return usersRepository.findAll(Sort.by("username"));
+    }
+
+    public Boolean createUser(UsersCreateDto usersCreateDto) {
+        Users user = new Users();
+        user.setUsername(usersCreateDto.getUsername());
+        if (groupRepository.findByName(usersCreateDto.getGroup()).isPresent()) {
+            user.setGroupId(groupRepository.findByName(usersCreateDto.getGroup()).get().getId());
+        }
+        user.setCreated(Timestamp.valueOf(LocalDateTime.now()));
+        user.setChanged(Timestamp.valueOf(LocalDateTime.now()));
+        Users savedUser = usersRepository.save(user);
+        return getUserById(savedUser.getId()).isPresent();
+    }
+
+    public Boolean updateUser(UsersUpdateDto usersUpdateDto) {
+        Optional<Users> optionalUser = usersRepository.findById(usersUpdateDto.getId());
+        if (optionalUser.isPresent()) {
+            Users user = optionalUser.get();
+            user.setUsername(usersUpdateDto.getUsername());
+            if (groupRepository.findByName(usersUpdateDto.getGroup()).isPresent()) {
+                user.setGroupId(groupRepository.findByName(usersUpdateDto.getGroup()).get().getId());
+            }
+            user.setChanged(Timestamp.valueOf(LocalDateTime.now()));
+            Users savedUser = usersRepository.save(user);
+            return savedUser.equals(user);
+        }
+        return false;
+    }
+
+    public Boolean updateUsersUsername(UsersUsernameUpdateDto usersUsernameUpdateDto) {
+        Optional<Users> optionalUser = usersRepository.findById(usersUsernameUpdateDto.getId());
+        if (optionalUser.isPresent()) {
+            Users user = optionalUser.get();
+            user.setUsername(usersUsernameUpdateDto.getUsername());
+            user.setChanged(Timestamp.valueOf(LocalDateTime.now()));
+            Users savedUser = usersRepository.save(user);
+            return savedUser.equals(user);
+        }
+        return false;
+    }
+
+    public Boolean updateUsersGroup(UsersUpdateGroupDto usersUpdateGroupDto) {
+        Optional<Users> optionalUser = usersRepository.findById(usersUpdateGroupDto.getId());
+        if (optionalUser.isPresent()) {
+            Users user = optionalUser.get();
+            if (groupRepository.findByName(usersUpdateGroupDto.getGroup()).isPresent()) {
+                user.setGroupId(groupRepository.findByName(usersUpdateGroupDto.getGroup()).get().getId());
+            }
+            user.setChanged(Timestamp.valueOf(LocalDateTime.now()));
+            Users savedUser = usersRepository.save(user);
+            return savedUser.equals(user);
+        }
+        return false;
+    }
+
+    public Boolean deleteUser(Long id) {
+        Optional<Users> optionalUser = usersRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            return false;
+        }
+        usersRepository.delete(optionalUser.get());
+        return true;
+    }
+}
