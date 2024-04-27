a4f0f9843d6e88c3aba52a99e0fb7c3684d6b23b
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-27 15:52:38 +0300

    users dtos created

---
+++ src/main/java/com/yumeinaruu/iis/model/dto/users/UsersCreateDto.java
@@ -0,0 +1,22 @@
+package com.yumeinaruu.iis.model.dto.users;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersCreateDto {
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String group;
+}
---
+++ src/main/java/com/yumeinaruu/iis/model/dto/users/UsersUpdateDto.java
@@ -0,0 +1,27 @@
+package com.yumeinaruu.iis.model.dto.users;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersUpdateDto {
+    @NotNull
+    @Min(1)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String group;
+}
