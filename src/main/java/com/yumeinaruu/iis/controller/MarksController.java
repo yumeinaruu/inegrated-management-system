b7de4d152f4eb6536beb1fe2aa3b400f66ff6762
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-27 16:33:55 +0300

    marks controller added

---
+++ src/main/java/com/yumeinaruu/iis/controller/MarksController.java
@@ -0,0 +1,123 @@
+package com.yumeinaruu.iis.controller;
+
+import com.yumeinaruu.iis.exception.custom_exception.CustomValidationException;
+import com.yumeinaruu.iis.model.Marks;
+import com.yumeinaruu.iis.model.dto.marks.MarksCreateDto;
+import com.yumeinaruu.iis.model.dto.marks.MarksMarkUpdateDto;
+import com.yumeinaruu.iis.model.dto.marks.MarksSubjectUpdateDto;
+import com.yumeinaruu.iis.model.dto.marks.MarksUpdateDto;
+import com.yumeinaruu.iis.model.dto.marks.MarksUserUpdateDto;
+import com.yumeinaruu.iis.service.MarksService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/marks")
+public class MarksController {
+    private final MarksService marksService;
+
+    @Autowired
+    public MarksController(MarksService marksService) {
+        this.marksService = marksService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Marks>> getAllMarks() {
+        List<Marks> marks = marksService.getAllMarks();
+        if (marks.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(marks, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Marks> getMarksById(@PathVariable Long id) {
+        Optional<Marks> marks = marksService.getMarkById(id);
+        if (marks.isPresent()) {
+            return new ResponseEntity<>(marks.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/ascending")
+    public ResponseEntity<List<Marks>> getMarksAscending() {
+        List<Marks> marks = marksService.getMarksAscending();
+        if (marks.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(marks, HttpStatus.OK);
+    }
+
+    @GetMapping("/descending")
+    public ResponseEntity<List<Marks>> getMarksDescending() {
+        List<Marks> marks = marksService.getMarksDescending();
+        if (marks.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(marks, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> createMarks(@RequestBody @Valid MarksCreateDto marksCreateDto,
+                                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(marksService.createMark(marksCreateDto) ? HttpStatus.CREATED : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> updateMarks(@RequestBody @Valid MarksUpdateDto marksUpdateDto,
+                                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(marksService.updateMark(marksUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/mark")
+    public ResponseEntity<HttpStatus> updateMarksMark(@RequestBody @Valid MarksMarkUpdateDto marksMarkUpdateDto,
+                                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(marksService.updateMarksMark(marksMarkUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/subject")
+    public ResponseEntity<HttpStatus> updateMarksSubject(@RequestBody @Valid MarksSubjectUpdateDto marksSubjectUpdateDto,
+                                                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(marksService.updateMarksSubject(marksSubjectUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<HttpStatus> updateMarksUser(@RequestBody @Valid MarksUserUpdateDto marksUserUpdateDto,
+                                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(marksService.updateMarksUser(marksUserUpdateDto) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteMarkById(@PathVariable Long id) {
+        return new ResponseEntity<>(marksService.deleteMarkById(id) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+}
