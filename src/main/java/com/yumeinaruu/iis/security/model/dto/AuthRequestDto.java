0f24cf718e1b909491cf9432d0ea3633b4dbc08f
Yumeinaruu <lisovoistas@yandex.ru>
2024-04-27 18:25:44 +0300

    validation

--- src/main/java/com/yumeinaruu/iis/security/model/dto/AuthRequestDto.java
+++ src/main/java/com/yumeinaruu/iis/security/model/dto/AuthRequestDto.java
@@ -1,12 +1,19 @@
 package com.yumeinaruu.iis.security.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class AuthRequestDto {
+    @NotNull
+    @Size(min = 1, max = 50)
     private String login;
+
+    @NotNull
+    @Size(min = 6, max = 50)
     private String password;
 }
 
--- src/main/java/com/yumeinaruu/iis/security/model/dto/RegistrationDto.java
+++ src/main/java/com/yumeinaruu/iis/security/model/dto/RegistrationDto.java
@@ -1,13 +1,26 @@
 package com.yumeinaruu.iis.security.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
 public class RegistrationDto {
+    @NotNull
+    @Size(min = 1, max = 50)
     private String login;
+
+    @NotNull
+    @Size(min = 6, max = 50)
     private String password;
+
+    @NotNull
+    @Size(min = 1, max = 50)
     private String username;
+
+    @NotNull
+    @Size(min = 1, max = 50)
     private String group;
 }
