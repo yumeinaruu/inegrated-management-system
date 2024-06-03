1f6342504d4544ce28f53b50800117bf78835d40
Yumeinaruu <lisovoistas@yandex.ru>
2024-06-04 00:16:12 +0300

    email attachments could be uploaded in swagger now

--- src/main/java/com/yumeinaruu/iis/controller/EmailSenderController.java
+++ src/main/java/com/yumeinaruu/iis/controller/EmailSenderController.java
@@ -8,11 +8,13 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,9 +37,9 @@ public class EmailSenderController {
             @ApiResponse(responseCode = "403", description = "You have no rights to this resource"),
             @ApiResponse(responseCode = "409", description = "Some error from your/our side")
     })
-    @PostMapping("/send")
+    @PostMapping(name = "/send", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPERADMIN')")
-    public ResponseEntity<HttpStatus> sendEmail(@RequestParam(value = "file", required = false) MultipartFile[] file,
+    public ResponseEntity<HttpStatus> sendEmail(@RequestPart(value = "file", required = false) MultipartFile[] file,
                                                 String to, String[] cc, String subject, String body) {
         return new ResponseEntity<>(emailService.sendEmail(file, to, cc, subject, body) ? HttpStatus.OK : HttpStatus.CONFLICT);
     }
